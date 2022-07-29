943386e2c39447be11649dedc56ab2353f0eb911
ConfidenceDev <confidostic3@gmail.com>
2022-07-29 23:58:59 +0100

    Stream module added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
+const Streamer = require("./stream");
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "views")));
@@ -11,28 +13,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/media", (req, res) => {
-  const range = req.headers.range;
-  if (!range) res.status(400).send("Requires Range header");
-
-  const vidPath = "laugh.mp4";
-  const vidSize = fs.statSync(vidPath).size;
-
-  const chunk = Math.pow(10, 5); // -> 100KB
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + chunk, vidSize - 1);
-
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${vidSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  res.writeHead(206, headers);
-  const stream = fs.createReadStream(vidPath, { start, end });
-  stream.pipe(res);
+  const str = new Streamer("laugh.mp4", 5, req);
+  str.start();
+  //str.stop();
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port: 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
